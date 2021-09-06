0589fae71fe14bb5d9b4b3a02f327fdccf767872
r.akhmatnabiev <r.akhmatnabiev@bars-open.ru>
2021-09-06 23:00:58 +0300

    Добавьте файлы проекта.

---
+++ TestWebAspNet/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace TestWebAspNet.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Autor { get; set; }
+        public int Price { get; set; }
+
+    }
+}
